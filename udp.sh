118ab227fcf125fea5cf9f93b7d6552d90746be9
rak-art <138667029+rak-art@users.noreply.github.com>
2023-07-13 19:32:02 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -6,7 +6,7 @@ sudo apt update-upgrade
 sudo apt install wget
 
 #install
-wget https://raw.githubusercontent.com/rak-art/Ppppppppppverips/main/flood.py
+wget https://github.com/rak-art/Husssssssss/blob/main/main_edit.py
 
 #Running
 python3 main_edit.py
